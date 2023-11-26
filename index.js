630dec9237a4c06a1015d565c45338f7d6da1c3b
Sohan Singh <139552712+codysohan@users.noreply.github.com>
2023-11-26 12:20:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ app.use(cors());
 app.use(express.json()); // If you want to use (req.body) You have to use this middlewere
 
 // Available Routes
-app.use("/api/auth", require("../routes/auth.js"));
-app.use("/api/notes", require("../routes/notes.js"));
+app.use("/api/auth", require("./routes/auth.js"));
+app.use("/api/notes", require("./routes/notes.js"));
 
 // Adding this for hosting purpose 
 app.use(express.static(path.join(__dirname, './inotebook/build')));
