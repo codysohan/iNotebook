69a385ffba2cf2e1abf4b4cbc2bb73b4c6351bae
Sohan <sohan8dec20051@gmail.com>
2023-11-26 11:53:33 +0530

    final code

---
+++ client/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const mongoURI = "mongodb://0.0.0.0:27017/inotebook";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI);
+    console.log('MongoDB has been successfully connected!');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+module.exports = connectToMongo;
---
+++ client/index.js
@@ -0,0 +1,28 @@
+const connectToMongo = require("./db.js");
+const express = require("express");
+var cors = require("cors");
+// Adding this for hosting purpose 
+const path = require('path');
+
+connectToMongo();
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json()); // If you want to use (req.body) You have to use this middlewere
+
+// Available Routes
+app.use("/api/auth", require("../routes/auth.js"));
+app.use("/api/notes", require("../routes/notes.js"));
+
+// Adding this for hosting purpose 
+app.use(express.static(path.join(__dirname, './inotebook/build')));
+app.get('*', function(req, res){
+  res.sendFile(path.join(__dirname, './intebook/build/index.html'))
+})
+
+// app.get() Commenting this for hosting purpose
+
+app.listen(port, () => {
+  console.log(`iNotebook backend started listening on port ${port}`);
+});
